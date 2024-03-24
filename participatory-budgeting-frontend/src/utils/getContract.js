60e0efd7d849c9b3285c7b3fd0b21601c2f2a6ff
Jeremy Jin <jeremy@Jeremys-MBP.lan>
2024-03-24 21:52:30 +0100

    Develop functions to show all the projects proposed and count the votes

--- participatory-budgeting-frontend/src/utils/getContract.js
+++ participatory-budgeting-frontend/src/utils/getContract.js
@@ -2,7 +2,9 @@ import Web3 from 'web3';
 import ParticipatoryBudgetingABI from '../contracts/ParticipatoryBudgetingABI.json';
 
 // const contractAddress = '0xc9CDcE80a136cdAFD65AfFaEE3d408808F00486f';
-const contractAddress = '0x50d5837B447c5041b2fC15e2d4A1E70a9E539229';
+// const contractAddress = '0x50d5837B447c5041b2fC15e2d4A1E70a9E539229';
+// const contractAddress = '0x25a01Ccc4c242F6F6dc7B73e68d44f92f4D47F30';
+const contractAddress = '0xe5428901FDd64832344C044Ea4cf9eC8360D4000';
 let participatoryBudgetingContract = null;
 
 export const getContract = () => {
